b3193e7ad7a2dea2a87786ac77676864bb22ef24
sarmila26 <44719381+sarmila26@users.noreply.github.com>
2021-06-01 19:46:21 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+
+          <DOCTYPE html>
+            <head>
+            </head>
+            <body>
+              Menghitung luas persegi panjang</br>
+              
+              Panjang : <input type="text" id="txtPanjang" name="txtPanjang"><br/>
+              Lebar : <input type="text" id="txtLebar" name="txtLebar"><br/>
+              <input type="button" id="btnHitung" value="HitungLuas"><br/>
+              Luas : <input type="text" id="txtLuas" name="txtLuas">
+            </body>
+          </html>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
